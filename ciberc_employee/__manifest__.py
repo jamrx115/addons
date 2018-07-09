86b351286cde90c098b7b19eef60a30a2934eb8f
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2018-07-09 09:09:51 -0500

    Agregar listas de país, ciudad, departamento, provincia, estado

--- ciberc_employee/__manifest__.py
+++ ciberc_employee/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml'
+        'views/views.xml',
+        'data/data.xml',
     ]
 }
--- ciberc_employee/models/models.py
+++ ciberc_employee/models/models.py
@@ -18,9 +18,18 @@ class ciberc_employee(models.Model):
     x_hide_state = fields.Boolean(string='Hide', compute="_compute_hide_country_id")
     x_hide_city = fields.Boolean(string='Hide', compute="_compute_hide_state_id")
 
+    x_pais_nace = fields.Many2one("res.country", string="País nacimiento", ondelete='restrict')
+    x_birth_state_id = fields.Many2one("res.country.state", string='Estado/Departamento', ondelete='restrict')
+    x_birth_city_id = fields.Many2one("ciberc.city", string='Ciudad', ondelete='restrict')
+    x_hide_birth_state = fields.Boolean(string='Hide', compute="_compute_hide_birth_country_id")
+    x_hide_birth_city = fields.Boolean(string='Hide', compute="_compute_hide_birth_state_id")
+
+    # Pais Estado Ciudad Reside
     @api.multi
     @api.onchange('x_pais_reside')
     def _onchange_country_id(self):
+        self.x_state_id = False
+        self.x_city_id = False
         if self.x_pais_reside:
             return {'domain': {'x_state_id': [('country_id', '=', self.x_pais_reside.id)]}}
         else:
@@ -50,6 +59,42 @@ class ciberc_employee(models.Model):
             else:
                 record.x_hide_city = True
 
+    # Pais Estado Ciudad Nacimiento
+    @api.multi
+    @api.onchange('x_pais_nace')
+    def _onchange_birth_country_id(self):
+        self.x_birth_state_id = False
+        self.x_birth_city_id = False
+        if self.x_pais_nace:
+            return {'domain': {'x_birth_state_id': [('country_id', '=', self.x_pais_nace.id)]}}
+        else:
+            return {'domain': {'x_birth_state_id': []}}
+
+    @api.depends('x_pais_nace')
+    def _compute_hide_birth_country_id(self):
+        for record in self:
+            if record.x_pais_nace:
+                record.x_hide_birth_state = False
+            else:
+                record.x_hide_birth_state = True
+
+    @api.multi
+    @api.onchange('x_birth_state_id')
+    def _onchange_birth_state_id(self):
+        if self.x_birth_state_id:
+            return {'domain': {'x_birth_city_id': [('state_id', '=', self.x_birth_state_id.id)]}}
+        else:
+            return {'domain': {'x_birth_city_id': []}}
+
+    @api.depends('x_birth_state_id')
+    def _compute_hide_birth_state_id(self):
+        for record in self:
+            if record.x_birth_state_id:
+                record.x_hide_birth_city = False
+            else:
+                record.x_hide_birth_city = True
+
+    # Regular expressions
     @api.multi
     @api.onchange('x_identificacion')
     def _check_value_identification(self):
