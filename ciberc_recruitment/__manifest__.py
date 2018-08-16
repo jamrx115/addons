a5bb1614fd479b5d7f827aff47cdee34ec6ed7f1
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2018-08-16 09:10:53 -0500

    Creación grupo solicitantes

--- ciberc_recruitment/__manifest__.py
+++ ciberc_recruitment/__manifest__.py
@@ -23,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/hr_recruitment_security.xml',
         'views/views.xml',
         'views/qweb_templates.xml',
     ]
