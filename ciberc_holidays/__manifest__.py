9b673f0be73928e6579ae0b39937f7f62c98db3b
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2018-09-10 17:35:43 -0500

    modificando reporte pdf de ausencias

---
+++ ciberc_holidays/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CIBERC - Holidays custom module",
+
+    'summary': """
+        Campos adicionales al modelo de ausencias""",
+
+    'description': """
+        Campos adicionales al modelo de ausencias
+    """,
+
+    'author': "Alltic SAS",
+    'website': "https://www.alltic.co",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'hr',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr','hr_holidays'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ]
+}
