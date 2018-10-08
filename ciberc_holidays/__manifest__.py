632df315b5f6488a94e04b5d27ad841e3b14da41
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2018-10-08 17:06:54 -0500

    Código en tipo de ausencia - para reglas salariales

--- ciberc_holidays/__manifest__.py
+++ ciberc_holidays/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr','hr_holidays'],
+    'depends': ['base','hr','hr_holidays', 'hr_payroll'],
 
     # always loaded
     'data': [
