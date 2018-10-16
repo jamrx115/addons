971eb7d843cf71f1fdc6740354583b3b75d3c0ff
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2018-10-16 11:53:53 -0500

    Módulo hr_contract_accessibility

---
+++ hr_contract_accessibility/README.rst
@@ -0,0 +1,12 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==========================
+HR Contracts Accessibility
+==========================
+
+
+HR Contracts Accessibility
+
+
---
+++ hr_contract_accessibility/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "HR Contracts Accessibility",
+    'summary': """HR Contracts Accessibility""",
+    'author': "Onestein",
+    'website': "http://www.onestein.eu",
+    'category': 'Human Resources',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_contract',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/hr_user_own.xml',
+    ],
+}
