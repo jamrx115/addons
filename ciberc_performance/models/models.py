394a6aa808bde79787b336c227444e87cddd9943
Oscar Rengifo <oscarrengifo089@gmail.com>
2018-10-25 19:01:19 -0500

    (oscar) - Mejoras al modulo de desempeño - boton de carga de objetivos

--- ciberc_performance/models/models.py
+++ ciberc_performance/models/models.py
@@ -22,55 +22,9 @@ class ciberc_performance (models.Model):
     general_rating = fields.Selection([('excepcional', 'Excepcional'),('sobresaliente', 'Sobresaliente'),('bueno', 'Bueno'),('mejor', 'Necesita mejorar'),],string="Calificación general", track_visibility="onchange")
     comments_extra = fields.Text(string="Comentarios adicionales")
     department_id = fields.Many2one('hr.department', related="name.employee_ids.department_id", string="Departamento")
-    state = fields.Selection([('borrador','Borrador'),('primer','Primer semestre'),('segundo','Segundo semestre'),('finalizada','Finalizada')], string="Estado", track_visibility="onchange",default="borrador")
+    state = fields.Selection([('borrador','Borrador'),('primer','Primera fase'),('segundo','Segunda fase'),('finalizada','Finalizada')], string="Estado", track_visibility="onchange",default="borrador")
     loaded = fields.Boolean(string="Cargado?", default=False)
 
-    """
-    # Comentado para subirlo a QA
-    @api.onchange('name')
-    def _onchange_goal_id(self):
-        Obj= self.env['res.partner'].search([('name', '=', self.name)])
-
-        return {'domain': {'goal_id': [('departments_ids', '=', self.name.employee_ids.department_id.id )]}}
-	"""
-    """
-    # Comentado para subirlo a QA
-    @api.multi
-    @api.onchange('name')
-    def _onchange_goal_id(self):
-
-        if self.name:
-           return {'domain': {'performance_goal_id.goal_id': [('performance_goal_id.goal_id.departments_ids', '=', self.name.employee_ids.department_id.id )]}}
-        else:
-            return {'domain': {'performance_goal_id.goal_id': []}}
-    """
-
-    """
-    # Comentado para subirlo a QA
-    @api.multi
-    @api.onchange('name')
-    def _onchange_goal_id(self):
-        if self.name:
-           return {'domain': {'goal_id': [('goal_id.departments_ids', '=', 'name.employee_ids.department_id')]}}
-        else:
-            return {'domain': {'goald_id': []}}
-    """
-
-    """
-    #prueba para el cambio de objetivos a partir del cambio del nombre
-    @api.onchange('name')
-    def _onchange_goals(self):
-        if self.name == self. 
-        self.performance_goal_id.goal_id.name = self.name.employee_ids.department_id
-    """
-    """
-    @api.multi
-    def _get_goals(self):
-        for line in used_car_ids:
-            result.append((0, 0, {'first_semester': line.first_semester, 'second_semester': line.second_semester}))
-        self.performance_goal_id = result
-    """
-    _logger.debug('.... Ingresó al metodo')
     @api.onchange('name')
     def _onchange_employee_department(self):
         self.department_id = self.name.employee_ids.department_id
@@ -101,31 +55,42 @@ class ciberc_performance (models.Model):
         values['state'] = 'primer'
         res = super(ciberc_performance,self.with_context(mail_create_nosubscribe=True)).create(values)
         return res
-
+    
     @api.multi
     def get_goal(self):
+        """
         goal_list = []
         goal_exist_list = []
         goal_set_list = []
-
+        self.env['ciberc.performance.line'].search([('ciberc_performance_id.id', '=', self.id)]).unlink()
+        #goals_of_this_employee = self.env['ciberc.goals'].search([('departments_ids', '=', self.department_id.id),('id', 'not in', self.performance_goal_id.goal_id.id)])
         goals_of_this_employee = self.env['ciberc.goals'].search([('departments_ids', '=', self.department_id.id)])
         goals_set_in_this_employee = self.env['ciberc.performance.line']
+
         for goal_exist in goals_of_this_employee:
             goal_exist_list.append(goal_exist.id)
+
         for goal_set in goals_set_in_this_employee:
-            goal_set_list.append(goal_set.goal_id)
+            goal_set_list.append(goal_set.goal_id.id)
+
         for goal in goal_exist_list:
-            if goal not in goal_set_list:
-                goal_created  = self.env['ciberc.performance.line'].create({
-                            'goal_id': goal})
+            if not self.performance_goal_id:
+                for goal2 in self.performance_goal_id.ids:
+                    if goal != goal2:
+                        goal_created  = self.env['ciberc.performance.line'].create({'goal_id': goal})
+                        goal_list.append(goal_created.id)
+            else:
+                goal_created  = self.env['ciberc.performance.line'].create({'goal_id': goal})
                 goal_list.append(goal_created.id)
         self.performance_goal_id = goal_list
-
     """
+
+    
     #FUNCIONA BIEN PERO QUIERO AGREGARLE UN IF PARA QUE NO CARGUE VARIAS VECES LOS MISMOS
     @api.multi
     def get_goal(self):
         goal_list = []
+        self.env['ciberc.performance.line'].search([('ciberc_performance_id.id', '=', self.id)]).unlink()
         goals_of_this_employee = self.env['ciberc.goals'].search([('departments_ids', '=', self.department_id.id)])
         for goal in goals_of_this_employee:
             goal_created  = self.env['ciberc.performance.line'].create({
@@ -133,7 +98,7 @@ class ciberc_performance (models.Model):
             goal_list.append(goal_created.id)
         self.performance_goal_id = goal_list
         return goal_list
-    """
+    
 
 
     """    
@@ -174,8 +139,8 @@ class ciberc_performance_line (models.Model):
     main_goal_id = fields.Many2one('ciberc.main.goals',related="goal_id.main_goal_related_id", string="objetivo general")
     #main_goal_id = fields.Many2one('ciberc.main.goals', string="objetivo general")
     personal_goal = fields.Char(string="Meta personal")
-    first_semester = fields.Char(string="Primer semestre")
-    second_semester = fields.Char(string="Segundo semestre")
+    first_semester = fields.Char(string="Primera fase")
+    second_semester = fields.Char(string="Segunda fase")
     score = fields.Char(string="Calificación")
     comments_employee = fields.Text(string="Comentarios empleado")
     comments_evaluator = fields.Text(string="Comentarios evaluador")
