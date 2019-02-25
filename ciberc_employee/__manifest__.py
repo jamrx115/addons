6bc31a21bd35855b4d92d3e1dbb91e99e5b0e43f
Diana Carolina Hernandez <diana.hernandez.abahonza@correounivalle.edu.co>
2019-02-25 09:05:08 -0500

    Congelando campo responsable

--- ciberc_employee/__manifest__.py
+++ ciberc_employee/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr','hr_employee_updation'],
+    'depends': ['base','hr','hr_employee_updation', 'ciberc'],
 
     # always loaded
     'data': [
